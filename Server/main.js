0fa7486210737aa64161dd195c744373063ad0c1
Aldo Troiano <aldot99@gmail.com>
2019-11-13 17:01:06 +0000

    Added support for periodic transmission of message to the clients.  An temp array is filled with team players and the team is only sent to the players part of the team. Working. Need to improve first transmission of UDP packet to ensure packet is correctly sent.

--- Server/main.js
+++ Server/main.js
@@ -1,6 +1,5 @@
 var client_listener = require('./handshake_listener.js');
 var db_con = require('./database_connection.js');
-var udp = require('./udp.js');
 var teams = require('./teams.js')
 
 db_con.create_connection()
